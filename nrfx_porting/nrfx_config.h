ef8ad24cd619afe47117c03fe248bbcc73f943fc
Bjørlo, Aurora <aurora.sletnes@nordicsemi.no>
2021-04-07 15:06:47 +0200

    Add necessary files for nrfx porting

---
+++ nrfx_porting/nrfx_config.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Nordic Semiconductor ASA. All Rights Reserved.
+ *
+ * The information contained herein is confidential property of Nordic Semiconductor ASA.
+ * The use, copying, transfer or disclosure of such information is prohibited except by
+ * express written agreement with Nordic Semiconductor ASA.
+ */
+
+#ifndef NRFX_CONFIG_H__
+#define NRFX_CONFIG_H__
+
+/* No config needed at the moment */
+
+#define NRFX_UARTE_ENABLED 1
+#define NRFX_UARTE0_ENABLED 1
+#define NRFX_PRS_ENABLED 0
+
+#endif
